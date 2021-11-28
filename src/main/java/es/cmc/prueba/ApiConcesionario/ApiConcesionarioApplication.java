ac242bc2d6fb58ec66785021a02c243bf921a019
david <david.garcia.monterroso@externos.gloval.es>
2021-11-28 14:13:07 +0100

    Commit inicial

---
+++ src/main/java/es/cmc/prueba/ApiConcesionario/ApiConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package es.cmc.prueba.ApiConcesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConcesionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConcesionarioApplication.class, args);
+	}
+
+}
